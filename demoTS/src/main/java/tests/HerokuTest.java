72f80edc0f66f8f878b3a21efb7be43e0f0e54dc
luis <lmontes@technologylatam.com>
2024-11-18 14:42:03 -0300

    cambios

--- demoTS/src/main/java/tests/HerokuTest.java
+++ demoTS/src/main/java/tests/HerokuTest.java
@@ -1,56 +1,64 @@
 package tests;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
-import org.testng.annotations.*;
-import pages.DemoTsPage;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-
+import pages.HerokuDropdownPage;
+import pages.HerokuPage;
+import pages.HerokuCheckBoxPage;
 
 
 public class HerokuTest {
-
     private WebDriver driver;
-    private static DemoTsPage demoTsPage;
+    private HerokuPage herokuPage;
+    private HerokuCheckBoxPage herokuCheckBoxPage;
+    private HerokuDropdownPage herokuDropdownPage;
+
     @BeforeClass
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-        demoTsPage = new DemoTsPage(driver);
+        driver = new ChromeDriver();
+        herokuPage = new HerokuPage(driver);
+        herokuCheckBoxPage = new HerokuCheckBoxPage(driver);
+        herokuDropdownPage = new HerokuDropdownPage(driver);
 
     }
 
-
     @Test
-    @Parameters("numeros")
-    public void heroku(@Optional String numeros) throws InterruptedException{
-
-
-
-        demoTsPage.abrirGoogle();
-        demoTsPage.clickKey();
-        demoTsPage.ingNumeros(numeros);
-        Thread.sleep(3000);
-
-
-
-
+    public void openHeroku() throws InterruptedException {
+        herokuPage.abrirHeroku();
+        Thread.sleep(2000);
+        herokuPage.clickCheckBoxes();
+        Thread.sleep(5000);
+        herokuCheckBoxPage.clickCheckBox1();
+        Thread.sleep(2000);
+        herokuCheckBoxPage.clickCheckBox2();
+        Thread.sleep(2000);
+        driver.navigate().back();
+        Thread.sleep(2000);
+        herokuPage.clickDropdown();
+        Thread.sleep(2000);
+        herokuDropdownPage.clickDropdown();
+        Thread.sleep(2000);
+        herokuDropdownPage.clickOpcion1();
+        Thread.sleep(2000);
+        herokuDropdownPage.clickDropdown();
+        Thread.sleep(2000);
+        herokuDropdownPage.clickOpcion2();
+        Thread.sleep(2000);
+        driver.navigate().back();
 
     }
 
     @AfterClass
     public void tearDown() {
-
         // Cierra el navegador
         if (driver != null) {
             driver.quit();
         }
     }
-
 }
